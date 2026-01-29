5215e53c4326fc6142bfb741c5d223811dad1482
Jack <jiaqizhao2074@gmail.com>
2026-01-29 23:50:16 +0000

    feat: requests 3 cryptos and refine output

--- data_preprocess/process.py
+++ data_preprocess/process.py
@@ -6,13 +6,23 @@ import json
 
 
 def main():
+    crypto_ids = {
+        'Bitcoin': 'bitcoin',
+        'Ethereum': 'ethereum',
+        'Tether': 'tether',
+    }
+    api_key = "6f077700b46dfbfee732c95d8aea6f8318369fcb1d766d403cc84d47c05b4fb1"
+    all_data = {}
+
     try:
-        # Bitcoin data
-        response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=6f077700b46dfbfee732c95d8aea6f8318369fcb1d766d403cc84d47c05b4fb1")
-        response_dict = response.json()
-        print(response_dict)
+        for name, crypto_id in crypto_ids.items():
+            url = f"https://rest.coincap.io/v3/assets/{crypto_id}?apiKey={api_key}"
+            response = requests.get(url)
+            response_dict = response.json()
+            all_data[name] = response_dict
+            print(json.dumps(response_dict, indent=4))
     except requests.RequestException:
-        print("Request Error")
+        sys.exit("Request Error")
 
 
 if __name__ == "__main__":
