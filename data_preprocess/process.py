f50dbdac4b7bbbb71f5c721218a1d51710dc1967
Jack <jiaqizhao2074@gmail.com>
2026-02-02 23:52:57 +0000

    feat: request history date

--- data_preprocess/process.py
+++ data_preprocess/process.py
@@ -4,9 +4,9 @@ import sys
 import os
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
-DAYS_TO_FETCH = 90
+DAYS_TO_FETCH = 1
 DATA_INTERVAL = "m15"  # mid-frequency
 
 
@@ -16,24 +16,41 @@ def main():
         'Ethereum': 'ethereum',
         'Tether': 'tether',
     }
-    
-    api_key = os.getenv("COINCAP_API_KEY")
-    if not api_key:
-        sys.exit("API key does not exist")
-        
-    all_data = {}
 
+    # Time
     current_time = datetime.now()
     start_time = get_ms(current_time - timedelta(days=DAYS_TO_FETCH))
     end_time= get_ms(current_time)
+    
+    # API
+    api_key = os.getenv("COINCAP_API_KEY")
+    if not api_key:
+        sys.exit("API key does not exist")
+    url = f"https://rest.coincap.io/v3/assets/{crypto_id}/history?apiKey={api_key}"
+        
+    # Request
+    request_headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Accept": "application/json",
+    }
+
+    request_parameters = {
+        "interval": DATA_INTERVAL,
+        "start": start_time,
+        "end": end_time,
+    }
+
+    all_data = {}
 
     try:
-        for name, crypto_id in crypto_ids.items():
-            url = f"https://rest.coincap.io/v3/assets/{crypto_id}/history?apiKey={api_key}"
-            response = requests.get(url)
-            response_dict = response.json()
-            all_data[name] = response_dict
-            print(json.dumps(response_dict, indent=4))
+        response = request.get(
+            url,
+            headers=request_headers,
+            params=request_parameters,
+        )
+        response_dict = response.json()
+        all_data[name] = response_dict
+        print(json.dumps(response_dict, indent=4))
     except requests.RequestException:
         sys.exit("Request Error")
 
