06e93fa78600db9928699e6025c41ac9f366b368
Jack <jiaqizhao2074@gmail.com>
2026-02-03 01:38:19 +0000

    fix: request error

--- data_preprocess/process.py
+++ data_preprocess/process.py
@@ -5,29 +5,29 @@ import os
 import requests
 import json
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-DAYS_TO_FETCH = 1
+DAYS_TO_FETCH = 0.01  # 1 interval for testing
 DATA_INTERVAL = "m15"  # mid-frequency
 
 
 def main():
-    crypto_ids = {
-        'Bitcoin': 'bitcoin',
-        'Ethereum': 'ethereum',
-        'Tether': 'tether',
-    }
+    load_dotenv()  # load .env
+
+    cryptos = ["bitcoin", "ethereum", "tether"]
 
     # Time
     current_time = datetime.now()
     start_time = get_ms(current_time - timedelta(days=DAYS_TO_FETCH))
-    end_time= get_ms(current_time)
-    
+    end_time = get_ms(current_time)
+    # print(start_time)
+    # print(end_time)
+
     # API
     api_key = os.getenv("COINCAP_API_KEY")
     if not api_key:
         sys.exit("API key does not exist")
-    url = f"https://rest.coincap.io/v3/assets/{crypto_id}/history?apiKey={api_key}"
-        
+
     # Request
     request_headers = {
         "Authorization": f"Bearer {api_key}",
@@ -40,24 +40,26 @@ def main():
         "end": end_time,
     }
 
-    all_data = {}
-
     try:
-        response = request.get(
-            url,
-            headers=request_headers,
-            params=request_parameters,
-        )
-        response_dict = response.json()
-        all_data[name] = response_dict
-        print(json.dumps(response_dict, indent=4))
+        for c in cryptos:
+            response = requests.get(
+                get_url(c),
+                headers=request_headers,
+                params=request_parameters,
+            )
+            response_json = response.json()
+            print(json.dumps(response_json, indent=4))
     except requests.RequestException:
         sys.exit("Request Error")
 
 
-# Used for API request 
+# Used for API request
 def get_ms(dt: datetime):
     return int(dt.timestamp() * 1000)
+
+
+def get_url(crypto_id: str):
+    return f"https://rest.coincap.io/v3/assets/{crypto_id}/history"
 
 
 if __name__ == "__main__":
