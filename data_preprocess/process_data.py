a80dcfb339131f2272efe8a5e6541c45558909b5
Jack <jiaqizhao2074@gmail.com>
2026-02-04 23:33:21 +0000

    feat: data aggregation

--- data_preprocess/process_data.py
+++ data_preprocess/process_data.py
@@ -7,6 +7,14 @@ def main():
     data["Timestamp"] = pandas.to_datetime(data["timestamp"])
     data = data.set_index("Timestamp").sort_index()
 
+    data_5min = data.resample("5T").agg({
+    "Open": "first",
+    "High": "max",
+    "Low": "min",
+    "Close": "last",
+    "Volume": "sum"
+})
+
 
 if __name__ == "__main__":
     main()
