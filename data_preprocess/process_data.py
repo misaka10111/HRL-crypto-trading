2406123f4f4e7a8f93120356add0cdd84e98f25b
Jack <jiaqizhao2074@gmail.com>
2026-02-06 09:47:47 +0000

    feat: udpate data range

--- data_preprocess/process_data.py
+++ data_preprocess/process_data.py
@@ -7,13 +7,17 @@ def main():
     data["Timestamp"] = pandas.to_datetime(data["timestamp"])
     data = data.set_index("Timestamp").sort_index()
 
-    data_5min = data.resample("5T").agg({
+    # Slice (start from 2017)
+    data = data.loc["2017-01-01":]
+
+    # Resample
+    data.resample("5T").agg({
         "Open": "first",
         "High": "max",
         "Low": "min",
         "Close": "last",
         "Volume": "sum"
-    }).dropna()
+    }).dropna().to_csv("btcusd_5-min_data.csv")
 
 
 if __name__ == "__main__":
