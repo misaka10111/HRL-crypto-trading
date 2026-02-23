fb8be513fd55c30c752bdb50e25f4d7a0239a717
Jack <jiaqizhao2074@gmail.com>
2026-02-23 16:12:18 +0000

    feat: update data preprocessor by dropping non-stationary features and adding cyclical time encoding

--- data_preprocess/build_features.py
+++ data_preprocess/build_features.py
@@ -1,11 +1,13 @@
-import pandas
+import pandas as pd
 import pandas_ta as ta
+import numpy as np
 
 def main():
-    input_file = "btcusd_5-min_data.csv"
-    output_file = "btcusd_5-min_features.csv"
-    data = pandas.read_csv(input_file, index_col="Datetime", parse_dates=True)
-        
+    input_file = "./data_preprocess/btcusd_5-min_data.csv"
+    output_file = "./data_preprocess/btcusd_5-min_features.csv"
+    data = pd.read_csv(input_file, index_col="Datetime", parse_dates=True)
+    data.index = pd.to_datetime(data.index)
+
     # RSI (Momentum)
     data.ta.rsi(length=14, append=True)
 
@@ -14,10 +16,27 @@ def main():
 
     # Bollinger Bands (Volatility)
     data.ta.bbands(length=20, std=2, append=True)
+    # BBL, BBM, BBU are absolute price, only keep BBP BBB
+    cols_to_drop = [col for col in data.columns if col.startswith(('BBL', 'BBM', 'BBU'))]
 
     # Log Return (as part of State)
     data.ta.log_return(length=1, append=True)
+    data['Vol_Change'] = data['Volume'].pct_change()
+    data['High_Low_Spread'] = (data['High'] - data['Low']) / data['Close']
 
+    # Time
+    time_of_day = data.index.hour + data.index.minute / 60.0
+    data['Time_Sin'] = np.sin(2 * np.pi * time_of_day / 24.0)
+    data['Time_Cos'] = np.cos(2 * np.pi * time_of_day / 24.0)
+    data['Day_Sin'] = np.sin(2 * np.pi * data.index.dayofweek / 7.0)
+    data['Day_Cos'] = np.cos(2 * np.pi * data.index.dayofweek / 7.0)
+
+    # drop not stationary features
+    cols_to_drop.extend(['Open', 'High', 'Low', 'Volume'])
+    data = data.drop(columns=cols_to_drop)
+
+    # drop NaN
+    data = data.replace([np.inf, -np.inf], np.nan)
     data = data.dropna()
     print(f"Generated Features: {data.columns.tolist()}")
 
